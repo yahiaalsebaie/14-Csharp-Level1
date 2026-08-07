da3c97bdb6d71b6d64f5c67230a4a353b81df874
Yahia Alsebaie <YahiaAlsebaie@gmail.com>
2026-08-07 20:53:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Csharp_Level1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
