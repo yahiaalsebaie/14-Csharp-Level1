ac58c633f1e21747f4c8fd595851905daeb65d05
Yahia Alsebaie <YahiaAlsebaie@gmail.com>
2026-08-07 21:06:34 +0300

    Hello World in C#

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _14_Csharp_Level1
+namespace Course_14_Csharp_Level1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World! THIS IS YAHIA");
+            Console.ReadKey();
         }
     }
 }
