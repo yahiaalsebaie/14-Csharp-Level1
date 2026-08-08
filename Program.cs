a677fd9f2d112cd3d79ff9c183dc154374871774
Yahia Alsebaie <YahiaAlsebaie@gmail.com>
2026-08-08 23:00:18 +0300

    C# - Anonymous Type In C#, an anonymous type is a type (class) without any name that can contain public read-only properties only. It cannot contain other members, such as fields, methods, events, etc.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 ﻿
-//ProgrammingAdvices.com
-//Mohammed Abu-Hadhoud
-
 using System;
 
 namespace Main
@@ -9,26 +6,53 @@ namespace Main
     internal class Program
     {
 
+
+
         static void Main(string[] args)
         {
 
-            //  Nullable<int> can be assigned any value
-            //  from -2147483648 to 2147483647, or a null value.
+            //you don't specify any type here , automatically will be specified
+            var student = new { Id = 20, FirstName = "Yahia", LastName = "Al-Sebaie" };
 
-            Nullable<int> i = null;
+            Console.WriteLine("\nExample1:\n");
+            Console.WriteLine(student.Id); //output: 20
+            Console.WriteLine(student.FirstName); //output: Yahia
+            Console.WriteLine(student.LastName); //output: Al-Sebaie
 
-            int? j = null;
-
-            int? grade = null;
-            Console.WriteLine(grade);
+            //You can print like this:
+            Console.WriteLine(student);
 
 
-            grade = 75;
-            Console.WriteLine(grade);
+            //anonymous types are read-only
+            //you cannot change the values of properties as they are read-only.
+
+            // student.Id = 2;//Error: cannot change value
+            // student.FirstName = "Ali";//Error: cannot change value
+
+
+            //An anonymous type's property can include another anonymous type.
+            var student2 = new
+            {
+                Id = 20,
+                FirstName = "Mohammed",
+                LastName = "Abu-Hadhoud",
+                Address = new { Id = 1, City = "Amman", Country = "Jordan" } //another anonymous type.
+            };
+
+            Console.WriteLine("\nExample2:\n");
+            Console.WriteLine(student2.Id);
+            Console.WriteLine(student2.FirstName);
+            Console.WriteLine(student2.LastName);
+
+            Console.WriteLine(student2.Address.Id);
+            Console.WriteLine(student2.Address.City);
+            Console.WriteLine(student2.Address.Country);
+            Console.WriteLine(student2.Address);
+
+
 
             Console.ReadKey();
 
         }
     }
 }
-
